5d17bd99ab28dcd992eeacb25a12c5b3da1793e3
TOEY <akanip2@gmail.com>
2021-09-18 23:55:56 +0700

    feat : add const.js

---
+++ src/global/consts.ts
@@ -0,0 +1,21 @@
+import joyseAvatar from "../assets/images/joyse.png";
+import samAvatar from "../assets/images/sam.png";
+import russellAvatar from "../assets/images/russell.png";
+
+export const CHAT_ROOM = [
+  { title: "General", id: "1" },
+  { title: "Technology", id: "2" },
+  { title: "LGTM", id: "3" },
+];
+
+export const ROOM_MAP = {
+  "1": "General",
+  "2": "Technology",
+  "3": "LGTM",
+};
+export const AVATAR_MAP = {
+  Joyse: joyseAvatar,
+  Sam: samAvatar,
+  Russell: russellAvatar,
+};
+export const USERS = ["Joyse", "Sam", "Russell"];
