f265ad46c1237f647b4f0d8e61982fa4d4bbff7b
TOEY <akanip2@gmail.com>
2021-09-20 12:33:52 +0700

    fix : correct date format

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,5 +1,7 @@
 export const getTimeFormat = (date?: string | undefined | null): string => {
   let d = new Date();
   if (date) d = new Date(date);
-  return `${d.getHours()}:${d.getMinutes()}`;
+  return `${String(d.getHours()).padStart(2, "0")}:${String(
+    d.getMinutes()
+  ).padStart(2, "0")}`;
 };
